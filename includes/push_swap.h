d7a17f6b58c1e985a96f457b70f61cd99c608731
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2023-11-25 17:00:57 +0900

    20231125

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,25 @@
 /*   By: akisuzuk <akisuzuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 01:00:58 by akisuzuk          #+#    #+#             */
-/*   Updated: 2023/11/19 01:17:38 by akisuzuk         ###   ########.fr       */
+/*   Updated: 2023/11/25 16:55:03 by akisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUAH_SWAP_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <math.h>
+//# include <climits> // ubuntuではいけたけどmacだと弾かれるな
+# include <limits.h>
+# include <stdbool.h>
+# include <stdarg.h>
+# include <fcntl.h>
+# include <signal.h>
+
+int		ft_atoi(const char *str);
+
+#endif
--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,9 +6,69 @@
 /*   By: akisuzuk <akisuzuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 00:59:14 by akisuzuk          #+#    #+#             */
-/*   Updated: 2023/11/19 01:00:50 by akisuzuk         ###   ########.fr       */
+/*   Updated: 2023/11/25 17:00:30 by akisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
+
+void sa ()
+{
+
+}
+
+void sb ()
+{
+
+}
+
+void ss ()
+{
+
+}
+
+void pa ()
+{
+
+}
+
+void pb ()
+{
+
+}
+
+void ra ()
+{
+
+}
+
+void rb ()
+{
+
+}
+
+void rr ()
+{
+
+}
+
+void rra ()
+{
+
+}
+
+void rrb ()
+{
+
+}
+
+void rrr ()
+{
+
+}
+
+main (int argc, *char argv[])
+{
+
+}
