dd7e5d2c81ea68cee24c46fb390c4eec27c1d34f
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2023-12-16 16:56:45 +0900

    zakkuri_frame

--- srcs/ps_actions.c
+++ srcs/ps_actions.c
@@ -1,29 +1,68 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   ps_actions.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akisuzuk <akisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/19 00:59:14 by akisuzuk          #+#    #+#             */
-/*   Updated: 2023/12/16 16:49:59 by akisuzuk         ###   ########.fr       */
+/*   Created: 2023/12/16 16:23:38 by akisuzuk          #+#    #+#             */
+/*   Updated: 2023/12/16 16:24:00 by akisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-// これ、pscine CPC12とかlibftのボーナスでやったリストの話っすね
-// argvをatoiしてint変数に格納
-// まずargcから変数の数を把握して、格納するリストのサイズを確定してmalloc
-int main (int argc, char *argv[])
+void	sa()
 {
-	int i;
 
-	i = 0;
-	result = ft_create_elem(&i_data1);
-	while(i < argc)
-	{
+}
 
-		i++;
-	}		
+void	sb()
+{
+
+}
+
+void	ss()
+{
+
+}
+
+void	pa()
+{
+
+}
+
+void	pb()
+{
+
+}
+
+void	ra()
+{
+
+}
+
+void	rb()
+{
+
+}
+
+void	rr()
+{
+
+}
+
+void	rra()
+{
+
+}
+
+void	rrb()
+{
+
+}
+
+void	rrr()
+{
+
 }
---
+++ srcs/ps_make_list.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ps_make_list.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akisuzuk <akisuzuk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/16 15:22:02 by akisuzuk          #+#    #+#             */
+/*   Updated: 2023/12/16 16:50:40 by akisuzuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// 全体的にpiscine CPC12のリストの問題を参照
+
+#include "../includes/push_swap.h"
+
+t_ps_list	*ft_create_elem(int num);
+
+t_ps_list	*ft_create_elem(int num)
+{
+	t_ps_list	*ret;
+
+	ret = (t_ps_list *)malloc(sizeof(t_ps_list));
+	(*ret).prev = null;
+	(*ret).next = null;
+	(*ret).data = data;
+	return (ret);
+	// もし最後の要素だったら、先頭のノードのprevに最後のノードを割り当てる
+}
+
+// スタック管理ということは、push_frontの処理でいい気がする。。。
+void	ft_list_push_front(t_ps_list **begin_list, void *data)
+{
+	t_ps_list	*front_elem;
+
+	front_elem = ft_create_elem(data);
+	(*front_elem).next = *begin_list;
+	*begin_list = front_elem;
+}
