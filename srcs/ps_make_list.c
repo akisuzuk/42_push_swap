f3fe4c35bdd11e60df960439dbf5927d6fbf1354
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2023-12-27 20:01:02 +0900

    func stc_a

--- srcs/ps_make_list.c
+++ srcs/ps_make_list.c
@@ -6,15 +6,13 @@
 /*   By: akisuzuk <akisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 15:22:02 by akisuzuk          #+#    #+#             */
-/*   Updated: 2023/12/17 15:55:43 by akisuzuk         ###   ########.fr       */
+/*   Updated: 2023/12/27 19:54:58 by akisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // 全体的にpiscine CPC12のリストの問題を参照
 
 #include "../includes/push_swap.h"
-
-t_ps_list	*ft_create_elem(int num);
 
 t_ps_list	*ft_create_elem(int num)
 {
@@ -23,12 +21,11 @@ t_ps_list	*ft_create_elem(int num)
 	ret = (t_ps_list *)malloc(sizeof(t_ps_list));
 	(*ret).prev = NULL;
 	(*ret).next = NULL;
-	(*ret).num = 0;
+	(*ret).value = num;
 	return (ret);
 	// もし最後の要素だったら、先頭のノードのprevに最後のノードを割り当てる
 }
 
-// スタック管理ということは、push_frontの処理でいい気がする。。。
 void	ft_list_push_front(t_ps_list **begin_list, void *data)
 {
 	t_ps_list	*front_elem;
@@ -36,4 +33,39 @@ void	ft_list_push_front(t_ps_list **begin_list, void *data)
 	front_elem = ft_create_elem(data);
 	(*front_elem).next = *begin_list;
 	*begin_list = front_elem;
+}
+
+void	ft_list_push_back(t_ps_list **begin_list, void *data)
+{
+	t_ps_list	*back_elem;
+	t_ps_list	*last;
+
+	back_elem = ft_create_elem(data);
+	last = ft_ps_list_last(*begin_list);
+	(*last).next = back_elem;
+}
+
+int	ft_ps_list_size(t_ps_list *begin_list)
+{
+	int		cnt;
+
+	cnt = 0;
+	while (begin_list != NULL)
+	{
+		begin_list = (*begin_list).next;
+		cnt++;
+	}
+	return (cnt);
+}
+
+t_ps_list	*ft_ps_list_last(t_ps_list *begin_list)
+{
+	t_ps_list	*prev_list;
+
+	while (begin_list != NULL)
+	{
+		prev_list = begin_list;
+		begin_list = (*begin_list).next;
+	}
+	return (prev_list);
 }
--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: akisuzuk <akisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 00:59:14 by akisuzuk          #+#    #+#             */
-/*   Updated: 2023/12/17 15:55:49 by akisuzuk         ###   ########.fr       */
+/*   Updated: 2023/12/27 19:45:41 by akisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,49 @@
 // これ、pscine CPC12とかlibftのボーナスでやったリストの話っすね
 // argvをatoiしてint変数に格納
 // まずargcから変数の数を把握して、格納するリストのサイズを確定してmalloc
-int main (int argc, char *argv[])
+
+t_ps_list	*make_stc_a(int c, char *v)
 {
 	int			i;
-	int			num;
-	t_ps_list	stc_a;
-	t_ps_list	stc_b;
+	t_ps_list	*ret;
 
 	i = 0;
-	// atoiでintに変換
-
-	// スタックAを生成
-	while(i < argc)
+	ret = ft_create_elem(ft_atoi(v[i]));
+	i++;
+	while (i < c)
 	{
-		if(i == 0)
-		{
-			num = atoi(argv[i + 1]);			
-			stc_a = ft_create_elem(num);
-		}
-		else
 		{	
-			ft_list_push_front();
+			ft_list_push_front(&ret, ft_atoi(v[i]));
+			i++;
 		}
 		i++;
 	}
-	// 同じサイズを持つストックBを作成
-	//stc_b = ft_create_elem();
-			
+}
+
+int	main(int argc, char **argv)
+{
+	int			i;
+	int			j;
+	int			num;
+	t_ps_list	*stc_a;
+	t_ps_list	*stc_b;
+
+	// 引数が条件を満たしているかチェック
+	i = 0;
+	j = 0;
+	while (i < argc - 1)
+	{
+		while (argv[i][j] != '\0')
+		{
+			if (ft_isdigit(argv[i][j++]) == 0)
+			{
+				write(2, "Error: Non-numeric values included.\n", 36);
+				exit (1);
+			}
+		}
+		i++;
+	}	
+	stc_a = make_stc_a (argc, argv);
+	stc_b = ;
+	return (0);
 }
